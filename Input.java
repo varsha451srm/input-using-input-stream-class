598ada82b7eacbd7b46549e6be12fded56e29764
varsha451srm <99247917+varsha451srm@users.noreply.github.com>
2023-02-25 14:40:48 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Input
+{
+ public static void main(String [] args)throws IOException
+  {
+    System.out.println("Enter a character:");
+    int a=System.in.read();
+    System.out.println("The character is " + a);
+ }
+}
+ 
